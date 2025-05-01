ce10a2692e48f54789a16990ea717f431c477474
AnneRom <coolann000.com@gmail.com>
2025-05-01 18:32:20 +0300

    Lesson_01-04
    Почали вивчати React (components + JSX)

---
+++ src/components/Product.jsx
@@ -0,0 +1,11 @@
+export default function Product() {
+    const price = 100;
+    return (
+    <>
+      <h2>Cookies</h2>
+      <img src="https://images.aws.nestle.recipes/resized/5b069c3ed2feea79377014f6766fcd49_Original_NTH_Chocolate_Chip_Cookie_1080_850.jpg" width="300px" alt="Cookies" />
+      <p>Price: {price} credits</p>
+    </>
+      );
+  }
+  
