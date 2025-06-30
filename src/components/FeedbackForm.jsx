fdbe542b0f2b8a08d346bfac2782614c6c7eb72b
AnneRom <coolann000.com@gmail.com>
2025-06-30 12:04:36 +0300

    Lesson_30-06
    в бібліотеці Formik розібрали компонент ErrorMessage

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,4 +1,4 @@
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import css from './FeedbackForm.module.css'
 import { useId } from 'react'
 import * as Yup from 'yup'
@@ -6,14 +6,14 @@ import * as Yup from 'yup'
 const FeedbackSchema = Yup.object().shape({
     username: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
     email: Yup.string().email("Must be a valid email!").required("Required"),
-    message: Yup.string().min(3, "Too short").max(256, "Too long").required("Required"),
-    selector: Yup.string().oneOf(["Option 1", "Option 2", "Option 3"]).required("Required")
+    message: Yup.string().min(3, "Too short").max(256, "Too long"),
+    selector: Yup.string().oneOf(["o1", "o2", "o3"]).required("Required")
 })
 
 const initialValues = {
         username: "",
         email: "hanna@com",
-        massage: "",
+        message: "",
         selector: ""
 }
 
@@ -28,23 +28,27 @@ const FeedbackForm = () => {
     const msgFieldId = useId()
     const selFieldId = useId()
 
-
     return (
       <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedbackSchema}>
         <Form className={css.form}>
             <label htmlFor={nameFieldId}>Username:</label>
             <Field id={nameFieldId} type="text" name="username" className={css.field}></Field>
+            <ErrorMessage name="username" component="span" className={css.error}/>
 
             <label htmlFor={emailFieldId}>Email:</label>
             <Field id={emailFieldId} type="email" name="email" className={css.field}></Field>
+            <ErrorMessage name="email" component="span" className={css.error}/>
 
-            <Field as="textarea" cols="20" rows="5" name="massage" id={msgFieldId}/>
+            <Field as="textarea" cols="20" rows="5" name="message" id={msgFieldId}/>
+            <ErrorMessage name="message" component="span" className={css.error}/>
 
             <Field as="select" name="selector" id={selFieldId}>
                 <option value="o1">Option 1</option>
                 <option value="o2">Option 2</option>
                 <option value="o3">Option 3</option>
             </Field>
+            <ErrorMessage name="selector" component="span" className={css.error}/>
+           
 
             <button type="submit" className={css.btn}>Submit</button>
         </Form>
